384083a021007db365a82c22cc8dbfaec85cce41
Konstantinos Dimitriou <konstantinos.dimitriou@aethon.sg>
2020-12-13 16:15:43 +0800

    Static homepage

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
-import * as sassMiddleware from 'node-sass-middleware';
+import * as compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(
@@ -12,14 +12,7 @@ async function bootstrap() {
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
-  app.use(
-    sassMiddleware({
-      src: join(__dirname, '..', 'scss'),
-      dest: join(__dirname, '..', 'public', 'css'),
-      debug: true,
-      outputStyle: 'compressed'
-    })
-  );
+  app.use(compression());
 
   await app.listen(3000);
 }
