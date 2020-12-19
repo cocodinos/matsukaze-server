84857bc0490cf905c062241635cd03f25b588442
Konstantinos Dimitriou <konstantinos.dimitriou@aethon.sg>
2020-12-19 17:05:57 +0800

    Language setting via GET queries

--- src/controllers/home/home.controller.ts
+++ src/controllers/home/home.controller.ts
@@ -1,16 +1,25 @@
-import { Get, Controller, Render } from '@nestjs/common';
+import { Get, Controller, Render, Query } from '@nestjs/common';
 import { I18n, I18nContext } from 'nestjs-i18n';
 
 @Controller("home")
 export class HomeController {
 
   @Get()
-  @Render('home')
-  async root(@I18n() i18n: I18nContext) {
-    const params = {
-      navbar: await i18n.translate("digenes.navbar"),
-      content: await i18n.translate("digenes.content.home")
+  @Render("home")
+  async root(@I18n() i18n: I18nContext, @Query() query ) {
+    var lang;
+    if(query.lang) { lang = "?lang=" + query.lang }
+    const viewParams = {
+      navbar: await i18n.translate("digenes.navbar", query.lang),
+      content: await i18n.translate("digenes.content.home", query.lang)
     }
-    return {splash: true, params: params};
+    return {url: "/home/", lang: lang, params: viewParams};
   }
+
+  @Get("order")
+  // @Render("home")
+  async order(@I18n() i18n: I18nContext) {
+    
+  }
+
 }
